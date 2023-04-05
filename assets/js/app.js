f73779b77959b6b4b1b4387090d2fa08c1a91729
Naveen Roy <naveenroy001@gmail.com>
2023-04-05 22:19:39 +0530

    update audio play

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@
 // var music = document.getElementById("audioPlayer");
 
 $(document).ready(function(){
+    document.getElementById("audioPlayer").play();
 //$('#audioPlayer').
     //video.play();
     //$('#content').sakura();
