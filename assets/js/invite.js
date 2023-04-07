2c37e060a4efc9fc3c06b253293be97fe71eab31
Naveen Roy <naveenroy001@gmail.com>
2023-04-07 18:29:14 +0530

    update details

---
+++ assets/js/invite.js
@@ -0,0 +1,11 @@
+import data from './invitation_data.json' assert { type: "json" };
+var siteUrl = 'https://naveenroy001.github.io/my-wedding-invitation?invite=';
+
+$(document).ready(function(){
+    console.log(data);
+    $('#invitationurl').html('');
+    
+    for(let i=0;i<data.length;i++) $('#invitationurl').append('<p><b>Name</b> : ' + data[i].name + ',<br><b>URL</b> : <a target="_blank" href="'+ siteUrl + data[i].url +'">'+ siteUrl + data[i].url +'</a></p>')    
+
+
+});
