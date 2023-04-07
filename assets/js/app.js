cd69a4fc75ad681cf2ec7a6ae46a81ee7dc364d3
Naveen Roy <naveenroy001@gmail.com>
2023-04-07 18:11:23 +0530

    update data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,40 @@
 // var video = document.getElementById("background-video");
 // var music = document.getElementById("audioPlayer");
 
+import data from './invitation_data.json' assert { type: "json" };
+
 $(document).ready(function(){
-    $(document).trigger('click');
-    
+    console.log(data);
+    let searchParams = new URLSearchParams(window.location.search);
+    if(searchParams.has('invite')){
+        let param = searchParams.get('invite');
+        $('#userInvite').removeClass('d-none');
+        $('#userInvite .nameclass').html(findData(param));
+    }
 });
 
 $(document).click(function(){
-    document.getElementById("audioPlayer").play();
+    //document.getElementById("audioPlayer").play();
 });
 
 $(window).scroll(function() {
-    document.getElementById("audioPlayer").play();
+   // document.getElementById("audioPlayer").play();
 });
 
-//Flower falling
+
+function findData(urlName){
+    return data.filter((d)=>d.url==urlName)[0].name;
+      
+}
+
+
+
+$('.open-button').click(function(){
+    $('#invitation-card').hide();
+
+
+});
+
+
+
+
